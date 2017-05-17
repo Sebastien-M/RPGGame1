933f195cbb48913220c163b40917c8231614bd7e
Sebastien <seb.mandaba@gmail.com>
2017-05-17 17:31:10 +0200

    isometric map generated

--- js/main.js
+++ js/main.js
@@ -6,13 +6,16 @@ let perso = {
     mp: 5,
 }
 let mapSquare = {
-    rotateX:"60deg",
+    rotateX: "60deg",
     rotateY: "0deg",
-    rotateZ:"-45deg",
-    color:"blue",
+    rotateZ: "-45deg",
+    color: "blue",
     width: "200px",
-    height: "200px"
-
+    height: "200px",
+    colors: ["#49b293", "#7df263", "#62f1b4", "#61cff0", "#608eef", "#9960ef"],
+    colorpicker: function () {
+        return colors[Math.floor((Math.random() * colors.length) + 0)];
+    }
 };
 //FORM
 document.body.querySelector("form").addEventListener("submit", function (e) {
@@ -27,23 +30,39 @@ document.body.querySelector("form").addEventListener("submit", function (e) {
         console.log("faded");
         console.log("Loading...")
         //CODE
-        for(let i = 0;i< 10;i++){
-            mapInit("div",mapSquare.rotateX,mapSquare.rotateY,mapSquare.rotateZ,"200px","200px","#49b293");
-        }
-        
+        // for (let l = 0; l < 50; l = l + 4) {
+        //     mapInit("div", mapSquare.rotateX, mapSquare.rotateY, mapSquare.rotateZ, "100px"
+        //     ,"100px", mapSquare.colors[Math.floor((Math.random() * mapSquare.colors.length) + 0)], l , 0);
+        // }
         //END
+        setMap();
         console.log("Loaded");
     }, 1000);
 
 });
-
-
 //FORM END
-function mapInit(element,Xrotate,Yrotate,Zrotate,h,w,col){
+function mapInit(element, Xrotate, Yrotate, Zrotate, h, w, col, l, t) {
     let mapgen = document.createElement(element);
     mapgen.style.height = h;
     mapgen.style.width = w;
-    mapgen.backgroundColor = col;
-    mapgen.style.transform = "rotateX("+Xrotate+") rotateY("+ Yrotate+") rotateZ("+ Zrotate+")";
+    mapgen.style.backgroundColor = col;
+    mapgen.style.transform = "rotateX(" + Xrotate + ") rotateY(" + Yrotate + ") rotateZ(" + Zrotate + ")";
+    //mapgen.style.position = "absolute";
+    mapgen.style.left = l + "%";
+    mapgen.style.top = t + "%";
     document.body.querySelector(".map").appendChild(mapgen);
+}
+//Math.floor((Math.random() * 10) + 1);
+function setMap() {
+    for (let i = 0; i < 99; i++) {
+        let div = document.createElement("div");
+        div.style.height = "50px";
+        div.style.width = "50px";
+        div.style.backgroundColor = mapSquare.colors[Math.floor((Math.random() * mapSquare.colors.length) + 0)];
+        div.style.border = "2px solid white";
+        div.className = "mapBlock";
+        div.id = "block" + i;
+        document.body.querySelector(".map").appendChild(div);
+
+    }
 }
